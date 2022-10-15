ac6f66595d2ca7a6bc131425d9798b51ea070bdb
Mattia <corradini.mattia05@gmail.com>
2022-10-15 15:10:06 +0200

    Aggiunge la calcolatrice

--- calcolatrice/main.js
+++ calcolatrice/main.js
@@ -5,8 +5,7 @@ function digita(n){
 function esegui() {
     var operazione = document.form.area_di_calcolo.value;
     if (operazione){
-        document.form.area_di_calcolo.value = eval(operazione); //La funzione eval valuta l’espressione 
-                                                         //passata come argomento e se c’è un’operazione la esegue
+        document.form.area_di_calcolo.value = eval(operazione); 
     }
 }
   
@@ -16,6 +15,5 @@ function resetta() {
   
 function cancella() {
     var s = document.form.area_di_calcolo.value;
-    document.form.area_di_calcolo.value = s.substring(0, s.length-1); //a.substring restituisce una parte della stringa 
-                                                                      //dalla pos richiesta
+    document.form.area_di_calcolo.value = s.substring(0, s.length-1); 
 }
