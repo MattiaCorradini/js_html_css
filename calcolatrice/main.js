da8ff077b94ffb00d99996376f816fe99529d163
Mattia <corradini.mattia05@gmail.com>
2022-11-05 09:47:29 +0100

    Aggiunge la calcolatrice

--- calcolatrice/main.js
+++ calcolatrice/main.js
@@ -1,18 +1,18 @@
 function digita(n){
     document.form.area_di_calcolo.value = document.form.area_di_calcolo.value + n
 }
-  
+
 function esegui() {
     var operazione = document.form.area_di_calcolo.value;
     if (operazione){
         document.form.area_di_calcolo.value = eval(operazione); 
     }
 }
-  
+
 function resetta() {
     document.form.area_di_calcolo.value = '';
 }
-  
+
 function cancella() {
     var s = document.form.area_di_calcolo.value;
     document.form.area_di_calcolo.value = s.substring(0, s.length-1); 
